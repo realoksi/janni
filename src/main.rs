11a91abe6675844e10b6c8f3d2dd9fc4f717f644
realoksi <15335458+realoksi@users.noreply.github.com>
2022-12-08 04:03:59 -0500

    wrote handler for slash commands and registering

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,53 @@ use std::env;
 
 use serenity::async_trait;
 use serenity::client::EventHandler;
+use serenity::model::application::interaction::{
+    Interaction, InteractionResponseType, InteractionType,
+};
 use serenity::model::prelude::*;
 use serenity::prelude::*;
 // use serenity::utils::token;
+
+mod application_commands;
 
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn ready(&self, _context: Context, ready: Ready) {
+    async fn ready(&self, context: Context, ready: Ready) {
+        command::Command::create_global_application_command(&context, |command| {
+            application_commands::ping::register(command)
+        })
+        .await
+        .expect("An error occurred creating global application commands.");
+
         println!(
             "{} ready, watching {} guild(s).",
             ready.user.name,
             ready.guilds.len()
         );
+    }
+
+    async fn interaction_create(&self, context: Context, interaction: Interaction) {
+        if interaction.kind().ne(&InteractionType::ApplicationCommand) {
+            return;
+        }
+
+        let application_command = interaction.application_command().unwrap();
+
+        let rename_me = match application_command.data.name.as_str() {
+            "ping" => application_commands::ping::run(&application_command.data.options),
+            _ => "NotImplemented".to_string(),
+        };
+
+        application_command
+            .create_interaction_response(&context.http, |response| {
+                response
+                    .kind(InteractionResponseType::ChannelMessageWithSource)
+                    .interaction_response_data(|message| message.content(rename_me))
+            })
+            .await
+            .expect("An error occurred responding to an application command.");
     }
 }
 
